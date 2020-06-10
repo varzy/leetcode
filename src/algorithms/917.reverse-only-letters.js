bdbc390be8b84242197d325004adb1fe8517d426
zy <varzy@live.com>
2020-06-10 15:43:28 +0800

    update

---
+++ src/algorithms/917.reverse-only-letters.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} S
+ * @return {string}
+ */
+var reverseOnlyLetters = function(S) {
+  const result = [];
+  const letters = S.split('').filter(item => {
+    const ascii = item.toUpperCase().charCodeAt();
+    return ascii >= 65 && ascii <= 90;
+  });
+
+  S.split('').map((letter, index) => {
+    const ascii = letter.toUpperCase().charCodeAt();
+    if (ascii >= 65 && ascii <= 90) {
+      result[index] = letters[letters.length - 1];
+      letters.pop();
+    } else {
+      result[index] = letter;
+    }
+  });
+
+  return result.join('');
+};
---
+++ src/workspace/91.decode-ways.js
@@ -0,0 +1,27 @@
+// Source: https://leetcode.com/problems/decode-ways/
+// Updated At: YEAR-MONTH-DAY
+
+// 需要使用递归
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = function(s) {
+  let result = 0;
+  for (let i = 1; i <= s.length; i++) {
+    const lPart = s.substring(0, i);
+    console.log(lPart);
+    if (lPart > 26) break;
+
+    for (let j = i; j < s.length; j++) {
+      const rPart = s.substring(i, j);
+      console.log(lPart);
+      if (rPart <= 26) result++;
+    }
+  }
+
+  return result;
+};
+
+console.log(numDecodings('12'));
